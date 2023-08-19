2b16d2cb7b35b1c47e33447f73dd0ff6ba06d142
Kishore <kishore@ficha.fr>
2023-08-19 11:12:15 +0530

    added sinple varaible script

---
+++ 02-varaibles.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo"$PERSON1: hi, $PERSON2 good morning"
+echo"$PERSON2: hi $PERSON1 good morning"
+echo"$PERSON1: How are you $PERSON2"
+echo"$PERSON2: I am fine $PERSON1 and what about you"
