39313eb70a63a065ac761840760b8e8bbb2eefc8
Kishore <kishore@ficha.fr>
2023-08-19 11:55:13 +0530

    added sinple varaible script

--- 06-varaibles.sh
+++ 06-varaibles.sh
@@ -5,3 +5,10 @@ echo "please enter the username"
 read USERNAME
 
 echo "The username you have entered is : $USERNAME"
+
+echo "please enter your password"
+
+read PASSWORD
+
+echo "The Password you have entered is : $PASSWORD"
+
