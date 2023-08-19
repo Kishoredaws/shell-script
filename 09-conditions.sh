a48de6045424c4fb0749428d6995b3eb9ede1c53
Kishore <kishore@ficha.fr>
2023-08-20 04:06:34 +0530

    conditions script added

---
+++ 09-conditions.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# The shell script for finding the number is greater than 10 or not?
+
+NUMBER=$1
+
+if [ NUMBER -gt 10 ]
+
+then
+
+echo "$NUMBER is greater than 10"
+
+else
+
+echo "$NUMBER is not greather than 10"
+
+fi
