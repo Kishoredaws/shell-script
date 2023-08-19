496d7415dd3293867a0750c71be9266bb2d81812
Kishore <kishore@ficha.fr>
2023-08-20 04:55:46 +0530

    installation pkgs script added

--- 10-mysql.sh
+++ 10-mysql.sh
@@ -20,3 +20,13 @@ exit 1
 else
 echo "The installation of postfix is success"
 fi 
+
+yum install mysql -y
+
+if [ $? -ne 0 ]
+then
+echo "The installation of mysql is having error"
+exit 1
+else
+echo "The installation of mysql is success"
+fi 
