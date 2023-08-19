0672d750992f0d29a62125266b99959a6596c16f
Kishore <kishore@ficha.fr>
2023-08-20 04:40:20 +0530

    installation pkgs script added

---
+++ 10- install pkgs.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# this program is to install packages 
+
+USERID=$(id -u)
+if [$USERID -ne 0 ]
+then
+echo "please run the script with root user"
+exit 1
+# else
+# echo "you are the root user"
+fi 
+
+yum install mysql -y
+
+if [$? -ne 0 ]
+then
+echo "The installation of mysql is having error"
+exit 1
+else
+echo "The installation of my sql is success"
+fi 
